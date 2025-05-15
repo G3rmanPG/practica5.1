993e16ac12cb37f6d644e8909152caad5d3d3512
G3rmanPG <germanpelaezgallardo@gmail.com>
2025-05-15 13:36:58 +0200

    conservando código anterior

--- Ejercicio2.scala
+++ Ejercicio2.scala
@@ -17,21 +17,68 @@ activa el sistema propuesto teniendo en cuenta los siguientes aspectos:
 - Se debe mostrar el nivel de agua al finalizar el programa (el cual deberá ser 0 tras los
   2000 incrementos y los 2000 decrementos). */
 object Lago{
+  @volatile var nvAgua = 0
+  val nvMax = 1000
+  val aumRio = 1000
+  val disPresa = 1000
+  val rios = 2
+  val presas = 2
+  val numProcesses = rios + presas //Se puede borrar sustituyendo por number.length
+  @volatile var number = Array.fill(numProcesses)(0) // Número de turno para cada proceso
+  @volatile var entering = Array.fill(numProcesses)(false) // Indica si un proceso está intentando entrar
   // Condición sincronización: esperan si el lago está vacío
   // Exclusión mutua sobre nivel
-  private var nivel = 0
+  /*private var nivel = 0
   @volatile private var fp0 = false // Si la presa 0 quiere tomar turno
   @volatile private var fp1 = false // Si la presa 1 quiere tomar turno
   @volatile private var turnop = 0 // Turno para cada presa
 
-  @volatile private var fp = false
+  @volatile private var fp = false // La presa quir
   @volatile private var fr = false // Si el río quiere tomar turno
   @volatile private var turnopr = 0 // turno presa = 0, turno río = 1
 
+  def incrementar() = {
+    fr = true
+    turnopr = 0
+    while(fp && turnopr == 0) Thread.sleep(0) //
+    nivel += 1
+    println(s"Nivel = $nivelAgua")
+    fr = false
+  }
 
+  def decrementar0() = {
+    while(nivel == 0) Thread.sleep(0)
+    fp0 = true
+    turnop = 1
+    while(fp1 && turnopr == 1) Thread.sleep(0)
+    // while(nivel == 0) Thread.sleep(0)
+    fp = true
+    turnopr = 1
+    while(fr && turnopr == 1)Thread.sleep(0)
+    nivel -= 1
+    println(s"Nivel = $nivelAgua")
+    fp = false
+    fp0 = false
+  }
+
+  def decrementar1() = {
+    fp1 = true
+    turnop = 0
+    while (fp0 && turnop == 0) Thread.sleep(0)
+
+    while (nivel == 0) Thread.sleep(0)
+    fp = true
+    turnopr = 0
+    while (fr && turnopr == 0) Thread.sleep(0)
+    nivel -= 1
+    log(s"Nivel = $nivel")
+    fp = false
+    fp1 = false
+  }
+  def nivelLago =  nivel */
 }
 object Ejercicio2 {
-  def main(args: Array[String]) = {
+  /*def main(args: Array[String]) = {
     val rio = new Thread(() =>{
       for(i <- 0 until 2000) // Incrementar nivel de agua 2000 veces
         Lago.incrementar()
@@ -48,10 +95,8 @@ object Ejercicio2 {
         Lago.incrementar()
     })
     rio.start(); presa0.start(); presa1.start()
-    rio.join()
-    presa0.join(); presa1.join()
+    rio.join(); presa0.join(); presa1.join()
     println(s"Nivel del lago = ${Lago.nivelLago}")
-  }
-
+  }*/
 
 }
